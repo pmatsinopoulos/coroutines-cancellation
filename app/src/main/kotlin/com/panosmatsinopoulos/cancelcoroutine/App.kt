9adb7acc3358ebb3afe8003056f1339442801cdd
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-07-24 17:29:08 +0300

    The coroutine does not cooperate and it cannot be canceled

--- app/src/main/kotlin/com/panosmatsinopoulos/cancelcoroutine/App.kt
+++ app/src/main/kotlin/com/panosmatsinopoulos/cancelcoroutine/App.kt
@@ -3,22 +3,24 @@
  */
 package com.panosmatsinopoulos.cancelcoroutine
 
-import kotlinx.coroutines.cancelAndJoin
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 fun main() {
     runBlocking {
-        val job = launch {
-            repeat(1_000) { i ->
-                println("job: I am sleeping $i ...")
-                delay(500L)
+        val startTime = System.currentTimeMillis()
+        val job = launch(Dispatchers.Default) {
+            var nextPrintTime = startTime
+            var i = 0
+            while (i < 5) {
+                if (System.currentTimeMillis() >= nextPrintTime) {
+                    println("job: I am sleeping ${i++}")
+                    nextPrintTime += 500L
+                }
             }
         }
         delay(1_300L)
-        println("main: I'm tired waiting")
+        println("main: I'm tired of waiting!")
         job.cancelAndJoin()
-        println("main. Now I can quit")
+        println("main: I can now quit")
     }
 }
