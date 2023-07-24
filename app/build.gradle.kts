544509ec291da67a388bed966d30b97d6cf73fa7
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-07-24 17:12:58 +0300

    Added the Kotlin coroutines dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.8.10"
+    id("org.jetbrains.kotlin.jvm") version "1.8.20"
 
     // Apply the application plugin to add support for building a CLI application in Java.
     application
@@ -28,6 +28,7 @@ dependencies {
 
     // This dependency is used by the application.
     implementation("com.google.guava:guava:31.1-jre")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
